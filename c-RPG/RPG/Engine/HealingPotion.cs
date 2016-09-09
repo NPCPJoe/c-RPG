08020f972143ab381f653e8686ab3bc921130a78
NPCPJoe <joecoo93@gmail.com>
2016-09-09 13:54:21 +0100

    Add files via upload

---
+++ c-RPG/RPG/Engine/HealingPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+   public class HealingPotion : Item // added item as the base class so all its info will be added to healing potion.
+    {
+        
+        public int AmountToHeal { get; set; }
+        public HealingPotion(int id, string name, string namePlural,
+ int amountToHeal) : base(id, name, namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
+    }
+}
---
+++ c-RPG/RPG/Engine/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+   public class Weapon : Item
+    {
+        public new int ID { get; set; }
+        public new string Name { get; set; }
+        public new string NamePlural { get; set; }
+        public int MinimumDamage { get; set; }
+ public int MaximumDamage { get; set; }
+        public Weapon(int id, string name, string namePlural,
+ int minimumDamage, int maximumDamage) :
+ base(id, name, namePlural)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+    }
+}
